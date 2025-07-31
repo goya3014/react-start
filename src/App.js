c6f81a9ea74ff8aa7b65ad78410b9986f658fae7
YUNMI LEE <goya3014@hanmail.net>
2025-07-31 00:14:06 -0700

    Added state to the component using the useState hook.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 function Header(props){
   return <header>
-          <h1><a href='/'>{props.title}</a></h1>
+          <h1><a href='/' onClick={(event)=>{
+            event.preventDefault();  
+            props.onChangeMode();
+          }}>{props.title}</a></h1>
         </header>
 }
 function Nav(props){
   const lis = []
   for(let i = 0; i < props.topics.length; i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+ t.id}>{t.title}</a></li>);
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+ t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(Number(event.target.id));
+      }}>{t.title}</a></li>);
   }
   return <nav>
         <ol>
@@ -24,16 +32,37 @@ function Article(props){
       </article>
 }
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
   const topics = [
     {id:1, title:'html', body: 'html is ...'},
     {id:2, title:'css', body: 'css is ...'},
     {id:3, title:'js', body: 'javascript is ...'},
   ]
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  }else if(mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i < topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
-      <Article title="Welcome" body="Hello, WEB"></Article>
+      <Header title="WEB" onChangeMode={()=>{
+        setMode('WELCOME');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        setMode('READ');
+        setId(id);
+      }
+      }></Nav>
+      {content}
     </div>
   );
 }
